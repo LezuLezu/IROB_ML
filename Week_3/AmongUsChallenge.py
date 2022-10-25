468a2ab6534656433a3aefa8a96f654d5f98ee86
Zoë <s1107714@student.hsleiden.nl>
2022-10-25 14:35:35 +0200

    started amongus challenge

--- Week_3/AmongUsChallenge.py
+++ Week_3/AmongUsChallenge.py
@@ -1,13 +1,33 @@
 import cv2 as cv
 import numpy as np
 import random
+import RPi._GPIO as GPIO
+from time import sleep
+
+
+# GPIO Setup
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+laserPIN = 40
+servoPIN = 7
+GPIO.setup(laserPIN, GPIO.OUT)
+GPIO.setup(servoPIN, GPIO.OUT)
+GPIO.output(laserPIN, GPIO.HIGH)
+
+
+def positionServo(angle):
+    pwm = GPIO.PWM(servoPIN, 50)
+    pwm.start(0)
+    duty = angle / 18 + 3
+    pwm.ChangeDutyCycle(duty)
+
 
 # Create video capture object and read from input file, if input is the camera, pass 0 instead of the video file name
 cap = cv.VideoCapture(0)
 template_1 = "img/crewmate_grey.jpeg"
 template_2 = "img/crewmate_red.jpeg"
 
-pathToTemplate = random.choise([template_1, template_2])
+pathToTemplate = random.choice([template_1, template_2])
 print(pathToTemplate)
 template = cv.imread(pathToTemplate, 0)
 w, h = template.shape[::-1]
@@ -33,6 +53,9 @@ else:
                 top_left = max_loc
                 bottom_right = (top_left[0] + w, top_left[1] + h)
                 cv.rectangle(frame, top_left, bottom_right, (0, 0, 255), 2)
+                positionServo(30)
+
+        
 
 
             # Display the resulting frame
@@ -41,6 +64,7 @@ else:
             if cv.waitKey(25) & 0xFF == ord('q'):
                 break
 
+        
         # Break the loop
         else:
             break
